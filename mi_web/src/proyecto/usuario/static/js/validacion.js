9838a75c2ac495e592807ae4c175b64a2825d504
vfernandezn <vfernandezn@uia.ac.cr>
2023-03-23 11:05:40 -0600

    Cambios

--- mi_web/src/proyecto/usuario/static/js/validacion.js
+++ mi_web/src/proyecto/usuario/static/js/validacion.js
@@ -3,27 +3,42 @@ function change(){
     var input = document.getElementById('identificacion');
     if (selectValue.value === 'Cédula'){
         input.oninput = function() {
+
             this.value = this.value.replace(/[^0-9]/g, '');
         }
-        input.minLength = 9;
-        input.maxLength = 9;
+        input.value = '';
+        input.removeAttribute('disabled');
+        input.setAttribute('minLength', '9');
+        input.setAttribute('maxLength', '9');
     }else if (selectValue.value === 'Dimex'){
+
         input.oninput = function() {
             this.value = this.value.replace(/[^0-9]/g, '');
         }
-        input.minLength = 12;
-        input.maxLength = 12;
-    }else if (selectValue.value === 'Pasaporte'){
-        input.minLength = 7;
-        input.maxLength = 7;
+        input.value = '';
+        input.removeAttribute('disabled');
+        input.setAttribute('minLength', '10');
+        input.setAttribute('maxLength', '12');
+    } else if (selectValue.value === 'Pasaporte'){
+
+        input.oninput = null;
+        input.value = '';
+        input.removeAttribute('disabled');
+        input.setAttribute('minLength', '9');
+        input.setAttribute('maxLength', '20');
     }
 
     else if (selectValue.value === 'Cédula Jurídica'){
+
         input.oninput = function() {
             this.value = this.value.replace(/[^0-9]/g, '');
         }
-        input.minLength = 11;
-        input.maxLength = 11;
+        input.value = '';
+        input.removeAttribute('disabled');
+        input.setAttribute('minLength', '11');
+        input.setAttribute('maxLength', '11');
     }
 }
+
+
 
