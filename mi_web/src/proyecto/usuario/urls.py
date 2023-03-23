1fd3d82009dcca61078d7cbeaef88c495d665d56
Minor Blanco Chaves <mblancoc@uia.ac.cr>
2023-03-23 09:38:32 -0600

    pass correct

--- mi_web/src/proyecto/usuario/urls.py
+++ mi_web/src/proyecto/usuario/urls.py
@@ -1,18 +1,17 @@
 from django.urls import path
 from .views import  Logueo, PaginaRegistroEstudiante,PaginaRegistroProfesor,CrearUsuario, DetalleUsuarioEstudiante, DetalleUsuarioProfesor, obtener_datos
 from django.contrib.auth.views import LogoutView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [path('', Logueo.as_view(), name='login'),
                path('registro_estudiantes/', PaginaRegistroEstudiante.as_view(), name='registro_estudiantes'),
                path('registro_profesor/', PaginaRegistroProfesor.as_view(), name='registro_profesor'),
                path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
                path('crear-usuario/', CrearUsuario.as_view(), name='crear-usuario'),
-
-               path('usuario/', DetalleUsuario.as_view(), name='usuario'),
-               path('password-reset/', PasswordResetView.as_view(template_name='registration/password-reset.html',html_email_template_name='registration/password-reset-email.html'),name='password-reset'),
-               path('password-reset-send/', PasswordResetDoneView.as_view(template_name='registration/password-reset-send.html'),name='password-reset-send'),
-               path('password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'),name='password-reset-confirm'),
-               path('password-reset-complete/',PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'),name='password-reset-complete'),   
+               path('password-reset/',auth_views.PasswordResetView.as_view(template_name='registration/password-reset.html',html_email_template_name='registration/password-reset-email.html'),name='password-reset'),
+               path('password-reset-send/', auth_views.PasswordResetDoneView.as_view(template_name='registration/password-reset-send.html'),name='password-reset-send'),
+               path('password-reset-confirm/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name='registration/password-reset-confirm.html'),name='password-reset-confirm'),
+               path('password-reset-complete/',auth_views.PasswordResetCompleteView.as_view(template_name='registration/password-reset-complete.html'),name='password-reset-complete'),   
 
 
                path('usuario-estudiante/', DetalleUsuarioEstudiante.as_view(), name='usuario_estudiante'),
