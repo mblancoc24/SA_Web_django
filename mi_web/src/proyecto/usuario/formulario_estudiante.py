cc55e1a0b6ce1f81183172c91d2f4eab4c5ef9cf
jlopezmUIA <jlopezm@uia.ac.cr>
2023-03-23 12:34:27 -0600

    Hola

--- mi_web/src/proyecto/usuario/formulario_estudiante.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import estudiantes
-
-class FormularioEstudiantes(forms.ModelForm):
-    class Meta:
-        model = estudiantes
-        fields = ('Cedula', 'nombre', 'primer_apellido',
-                  'segundo_apellido', 'fecha_nacimiento', 'phone_tutor', 
-                  'correo_estudiante', 'password', 'pago_realizado','documentos_presentados')
