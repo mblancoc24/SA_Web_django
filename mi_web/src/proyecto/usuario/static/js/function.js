e33c0b7258877cd2860cc32f101819beadc40aef
Minor Blanco Chaves <mblancoc@uia.ac.cr>
2023-04-11 14:07:46 -0600

    Upgrade icons

--- mi_web/src/proyecto/usuario/static/js/function.js
+++ mi_web/src/proyecto/usuario/static/js/function.js
@@ -49,6 +49,12 @@ function datacedula() {
         xhttp.send();
     }
 }
+$(function(){
+    $('#datepicker').datepicker({
+        uiLibrary: 'bootstrap5',
+        format: 'yyyy-mm-dd'
+    });
+});
 const loadingIndicator = document.getElementById('loading-indicator');
 loadingIndicator.classList.remove('hidden'); // Mostrar el indicador
 // Realizar la acción
