38a5c117e8a04c2be6a8be184917ba64a7e70cdc
vfernandezn <vfernandezn@uia.ac.cr>
2023-05-29 11:02:26 -0600

    Cambios

---
+++ mi_web/src/proyecto/usuario/static/js/formRegistros.js
@@ -0,0 +1,70 @@
+function llenardata() {
+    datacolegios();
+    datacarreras();
+}
+
+function datacarreras() {
+    var select = $("#mi_select");
+    select.empty(); // Eliminamos todas las opciones anteriores
+    var opcionPorDefecto = new Option("Seleccione una carrera", "", true, true);
+    select.append(opcionPorDefecto);
+
+    fetch("/carrerasselect/")
+        .then((response) => response.json())
+        .then((data) => {
+            // Iteramos sobre los valores obtenidos
+            data.forEach(function (valor) {
+                // Creamos una nueva opción y la agregamos al select
+                var opcion = new Option(valor, valor);
+                select.append(opcion);
+            });
+            select.select2({
+                dropdownParent: $("#modal_primer_ingreso"),
+                width: "100%", // Ancho del dropdown
+            });
+        });
+}
+
+function datacolegios() {
+    var select = $("#colegio_select");
+    select.empty(); // Eliminamos todas las opciones anteriores
+    var opcionPorDefecto = new Option("Seleccione un colegio", "", true, true);
+    select.append(opcionPorDefecto);
+    fetch("/colegiosselect/")
+        .then((response) => response.json())
+        .then((data) => {
+            // Iteramos sobre los valores obtenidos
+            data.forEach(function (valor) {
+                // Creamos una nueva opción y la agregamos al select
+                var opcion = new Option(valor, valor);
+                select.append(opcion);
+            });
+            select.select2({
+                dropdownParent: $("#modal_primer_ingreso"),
+                width: "100%", // Ancho del dropdown
+            });
+        });
+}
+
+function dataposgrados() {
+    var select = document.getElementById("posgradoselect");
+    select.options.length = 0; // Eliminamos todas las opciones anteriores
+    var opcionPorDefecto = new Option("Seleccione una posgrado", "", true, true);
+    select.append(opcionPorDefecto);
+
+    fetch("/posgradosselect/")
+        .then((response) => response.json())
+        .then((data) => {
+            // Iteramos sobre los valores obtenidos
+            data.forEach(function (valor) {
+                // Creamos una nueva opción y la agregamos al select
+                var opcion = new Option(valor, valor);
+                select.append(opcion);
+            });
+            // Inicializamos Select2 con el dropdownParent apuntando al modal
+            select.select2({
+                dropdownParent: $("#modal_posgrado"),
+                width: "100%", // Ancho del dropdown
+            });
+        });
+}
--- mi_web/src/proyecto/usuario/static/js/function.js
+++ mi_web/src/proyecto/usuario/static/js/function.js
@@ -74,3 +74,354 @@ function datacarreras() {
         });
     });
 }
+
+
+function checkOtro() {
+    // Obtener el valor seleccionado del select
+    var selectedValue = document.getElementById("genero").value;
+    // Si se seleccionó "Otro", mostrar el modal
+    if (selectedValue === "otro") {
+        $('#modal_inclusivo').modal('show');
+    }
+}
+
+function continuarGenero() {
+    var inputValue = document.getElementById("trato2").value;
+    var selectedValue = document.getElementById("tipoTrato").value;
+
+    document.getElementById("otro_trato").value = inputValue;
+    document.getElementById("otro_sexo").value = selectedValue;
+
+    $('#modal_inclusivo').modal('hide');
+}
+
+var provinciasCargadas = false;
+
+function cargarProvincias() {
+    if (provinciasCargadas) {
+        return;
+    }
+
+    var select = document.getElementById("provincia_select");
+
+    var url = "/obtener-provincia/";
+    var request = new XMLHttpRequest();
+    request.open('GET', url, true);
+    request.onload = function () {
+        if (request.status === 200) {
+            var data = JSON.parse(request.responseText);
+            select.options.length = 0;
+            for (key in data) {
+                var option = document.createElement("option");
+                option.value = key;
+                option.text = data[key];
+                select.add(option);
+            }
+            provinciasCargadas = true;
+            dataCantones(select);
+            obtenerProvincia(select)
+        }
+    };
+    request.send();
+}
+
+function cargarCantones(valor) {
+    var select = document.getElementById("canton_select");
+    var select2 = document.getElementById("distrito_select");
+
+    var url = "/obtener-canton/?provincia_select=";
+    var request = new XMLHttpRequest();
+    request.open('GET', url + encodeURIComponent(valor), true);
+    request.onload = function () {
+        if (request.status === 200) {
+            var data = JSON.parse(request.responseText);
+            select.options.length = 0;
+            for (key in data) {
+                var option = document.createElement("option");
+                option.value = key;
+                option.text = data[key];
+                select.add(option);
+            }
+            dataDistritos(select)
+            obtenerCanton(select)
+        }
+    };
+    request.send();
+}
+
+function cargarDistritos(valor1, valor2) {
+    var select = document.getElementById("distrito_select");
+
+    var url = "/obtener-distrito/?provincia_select=" + encodeURIComponent(valor1) + "&canton_select=" + encodeURIComponent(valor2);
+    var request = new XMLHttpRequest();
+    request.open('GET', url, true);
+    request.onload = function () {
+        if (request.status === 200) {
+            var data = JSON.parse(request.responseText);
+            select.options.length = 0;
+            for (key in data) {
+                var option = document.createElement("option");
+                option.value = key;
+                option.text = data[key];
+                select.add(option);
+            }
+            obtenerDistrito(select)
+        }
+    };
+    request.send();
+}
+
+var paisCargadas = false;
+
+function cargarPais() {
+    if (paisCargadas) {
+        return;
+    }
+
+    var select = document.getElementById("pais_select");
+
+    select.options.length = 0; // Eliminamos todas las opciones anteriores
+
+    var url = "/obtener-nacionalidad/";
+    var request = new XMLHttpRequest();
+    request.open('GET', url, true);
+    request.onload = function () {
+        if (request.status === 200) {
+            var data = JSON.parse(request.responseText);
+            for (key in data) {
+                var option = document.createElement("option");
+                option.value = key;
+                option.text = data[key];
+                select.add(option);
+            }
+            paisCargadas = true;
+        }
+    };
+    request.send();
+}
+
+function dataCantones(select) {
+    var valorSeleccionado = select.options[select.selectedIndex].value;
+    cargarCantones(valorSeleccionado);
+}
+
+function dataDistritos(select) {
+    const miSelect = document.getElementById('provincia_select');
+    var valorSeleccionado = select.options[select.selectedIndex].value;
+
+    cargarDistritos(miSelect.value, valorSeleccionado);
+}
+
+function obtenerPais(select) {
+    var textoSeleccionado = select.options[select.selectedIndex].text;
+    document.getElementById('pais').value = textoSeleccionado;
+}
+
+function obtenerProvincia(select) {
+    var textoSeleccionado = select.options[select.selectedIndex].text;
+    document.getElementById('provincia').value = textoSeleccionado;
+    dataCantones(select)
+}
+
+function obtenerCanton(select) {
+    var textoSeleccionado = select.options[select.selectedIndex].text;
+    document.getElementById('canton').value = textoSeleccionado;
+    dataDistritos(select)
+}
+
+function obtenerDistrito(select) {
+    var textoSeleccionado = select.options[select.selectedIndex].text;
+    document.getElementById('distrito').value = textoSeleccionado;
+}
+
+function mostrarAnterior() {
+    const tabActivo = document.querySelector(".tab-pane.active");
+    const tabAnterior = tabActivo.previousElementSibling;
+
+    if (tabAnterior) {
+        const linkActivo = document.querySelector(".nav-link.active");
+        const linkAnterior = linkActivo.parentElement.previousElementSibling.querySelector(".nav-link");
+
+        tabActivo.classList.remove("active");
+        tabActivo.classList.remove("show");
+        tabAnterior.classList.add("active");
+        tabAnterior.classList.add("show");
+
+        linkActivo.classList.remove("active");
+        linkAnterior.classList.add("active");
+    }
+}
+function mostrarSiguiente() {
+    const tabActivo = document.querySelector(".tab-pane.active");
+    const tabSiguiente = tabActivo.nextElementSibling;
+
+    if (tabActivo) {
+        const linkActivo = document.querySelector(".nav-link.active");
+        const linkSiguiente = linkActivo.parentElement.nextElementSibling.querySelector(".nav-link");
+
+        tabActivo.classList.remove("active");
+        tabActivo.classList.remove("show");
+        tabSiguiente.classList.add("active");
+        tabSiguiente.classList.add("show");
+
+        linkActivo.classList.remove("active");
+        linkSiguiente.classList.add("active");
+    }
+}
+
+var codigo = 0;
+var result;
+function enviarCodigo() {
+    var nombre = document.getElementById("nombre");
+    var primer_apellido = document.getElementById("primerapellido");
+    var segundo_apellido = document.getElementById("segundoapellido");
+    var nombreCompleto = nombre.value + ' ' + primer_apellido.value + ' ' + segundo_apellido.value;
+    console.log(nombreCompleto);
+    var correo = document.getElementById("correo");
+    console.log(correo.value);
+    var url = "/codigoVerificacion/?nombre=" + encodeURIComponent(nombreCompleto) + "&correo=" + encodeURIComponent(correo.value);
+
+    var request = new XMLHttpRequest();
+    request.open('GET', url, true);
+
+    request.onload = function () {
+        if (request.status === 200) {
+            codigo = JSON.parse(request.responseText);
+            console.log(codigo)
+        }
+    };
+    request.send();
+}
+
+function llamarFunciones() {
+    validatePasswords()
+    if (!result) {
+        if (codigo == 0) {
+            enviarCodigo();
+        }
+        mostrarSiguiente();
+    }
+}
+
+function moverInput(evento, siguiente) {
+    const tamano = evento.target.value.length;
+    const maximo = evento.target.getAttribute("maxlength");
+
+    if (tamano >= maximo) {
+        if (siguiente <= 4) {
+            document.getElementById("input" + siguiente).focus();
+        } else {
+            document.getElementById("input" + siguiente).blur();
+        }
+    }
+}
+
+function verificarCodigo() {
+    var input1 = document.getElementById('input1');
+    var input2 = document.getElementById('input2');
+    var input3 = document.getElementById('input3');
+    var input4 = document.getElementById('input4');
+    var butonRegistrar = document.getElementById('registrarEstudiante');
+    var codigoCompleto = input1.value + input2.value + input3.value + input4.value;
+    const resultado = codigoCompleto == codigo;
+    butonRegistrar.classList.toggle('disabled', !resultado);
+}
+
+$(document).ready(function () {
+    $('#tipo, #identificacion, #nombre, #primerapellido, #segundoapellido, #fechanacimiento, #telefono, #telefonoDos, #genero, #pais_select').on('change input', habilitarStep2);
+
+    $('#provincia_select, #distrito_select, #canton_select, #direccion_exacta').on('change input', habilitarStep3);
+
+    $('#correo, #password1, #password2').on('input', habilitarStep4);
+});
+
+function habilitarStep2() {
+    const botonSiguienteDatosP = document.getElementById('botonSiguienteDatosP');
+    const direccionUl = document.getElementById('direccionUl');
+    const tipoSelect = document.getElementById('tipo');
+    const identificacion = document.getElementById('identificacion');
+    const nombre = document.getElementById('nombre');
+    const primerApe = document.getElementById('primerapellido');
+    const segundoApe = document.getElementById('segundoapellido');
+    const fecha = document.getElementById('fechanacimiento');
+    const genero = document.getElementById('genero');
+    const telefono = document.getElementById('telefono');
+    const telefonoDos = document.getElementById('telefono2');
+    const nacionalidad = document.getElementById('pais_select');
+
+    const tipoValue = tipoSelect.value;
+    const identificacionValue = identificacion.value;
+    const nombreValue = nombre.value;
+    const primerApeValue = primerApe.value;
+    const segundoApeValue = segundoApe.value;
+    const fechaValue = fecha.value;
+    const generoValue = genero.value;
+    const telefonoValue = telefono.value;
+    const telefono2Value = telefonoDos.value;
+    const nacionalidadValue = nacionalidad.value;
+
+    const resultado = tipoValue && identificacionValue
+        && nombreValue && primerApeValue && segundoApeValue
+        && fechaValue && generoValue && telefonoValue
+        && telefono2Value && nacionalidadValue;
+
+    botonSiguienteDatosP.classList.toggle('disabled', !resultado);
+    direccionUl.classList.toggle('disabled', !resultado);
+}
+
+function habilitarStep3() {
+    const botonSiguienteDireccionP = document.getElementById('botonSiguienteDireccionP');
+    const correoUl = document.getElementById('correoUl');
+    const provincia = document.getElementById('provincia_select');
+    const canton = document.getElementById('canton_select');
+    const distrito = document.getElementById('tipo');
+    const direccionExacta = document.getElementById('direccion_exacta');
+    const provinciaValue = provincia.value;
+    const cantonValue = canton.value;
+    const distritoValue = distrito.value;
+    const direccionExactaValue = direccionExacta.value;
+    const resultado = provinciaValue && cantonValue && distritoValue && direccionExactaValue
+
+    botonSiguienteDireccionP.classList.toggle('disabled', !resultado);
+    correoUl.classList.toggle('disabled', !resultado);
+}
+
+function habilitarStep4() {
+    const botonSiguienteCorreoP = document.getElementById('botonSiguienteCorreoP');
+    codigo = 0;
+    const correo = document.getElementById('correo');
+    const contrasena = document.getElementById('password1');
+    const contrasena2 = document.getElementById('password2');
+    const correoValue = correo.value;
+    const contrasenaValue = contrasena.value;
+    const contrasena2Value = contrasena2.value;
+    const resultado = correoValue && contrasenaValue && contrasena2Value;
+
+    botonSiguienteCorreoP.classList.toggle('disabled', !resultado);
+}
+
+function validatePasswords() {
+    const passwordError = document.getElementById('message');
+    const passwordInput = document.getElementById('password1');
+    const passwordInput2 = document.getElementById('password2');
+    const password = passwordInput.value;
+    const password2 = passwordInput2.value;
+    const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_.:;,\-\+\]\[\}\{\¡\¿\'\?=\´~\"!°¬|])[A-Za-z\d!@#$%^&*()_.:;,\-\+\]\[\}\{\¡\¿\'\?=\´~\"!°¬|]{8,}$/;
+
+    if (password !== password2) {
+        passwordError.textContent = "Las contraseñas no coinciden";
+        result = true;
+        botonSiguienteCorreoP.classList.toggle('disabled', result);
+        validarUl.classList.toggle('disabled', result);
+    } else if (!passwordPattern.test(password)) {
+        passwordError.textContent = "La contraseña debe tener al menos 8 caracteres, una letra mayúscula, una letra minúscula, un número y un carácter especial.";
+        result = true;
+        botonSiguienteCorreoP.classList.toggle('disabled', result);
+        validarUl.classList.toggle('disabled', result);
+    } else {
+        passwordError.textContent = "";
+        result = false;
+        botonSiguienteCorreoP.classList.toggle('disabled', result);
+        validarUl.classList.toggle('disabled', result);
+    }
+}
